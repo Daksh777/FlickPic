0ddbc1bbe86628b27001d26c01357174eb245615
Daksh P. Jain <jaindaksh42@gmail.com>
2024-07-01 13:50:01 +0530

    Add post models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=500)
+    image = models.URLField(max_length=500)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.CharField(max_length=100, primary_key=True, unique=True, editable=False, default=uuid.uuid4)
+    
+    def __str__(self):
+        return str(self.title)
