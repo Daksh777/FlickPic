33eab5282d355f8c441cc9095409377ae19f3b31
Daksh P. Jain <jaindaksh42@gmail.com>
2024-07-02 13:07:10 +0530

    Implement Flickr web crawler

--- posts/models.py
+++ posts/models.py
@@ -3,7 +3,9 @@ import uuid
 
 class Post(models.Model):
     title = models.CharField(max_length=500)
+    artist = models.CharField(max_length=500, null=True)
     image = models.URLField(max_length=500)
+    url = models.URLField(max_length=500, null=True)
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     id = models.CharField(max_length=100, primary_key=True, unique=True, editable=False, default=uuid.uuid4)
--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from .models import *
 from django.forms import ModelForm
 from django import forms
+from bs4 import BeautifulSoup
+from django.contrib import messages
+import requests
 
 def home_view(request):
     posts = Post.objects.all()
@@ -10,14 +13,14 @@ def home_view(request):
 class PostCreateForm(ModelForm):
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = ['url', 'body']
         labels = {
-            'title': 'Title',
-            'image': 'Image URL',
             'body': 'Caption',
+            'url': 'Image URL',
         }
         widgets = {
             'body': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Write a caption...'}),
+            'url': forms.URLInput(attrs={'placeholder': 'Enter the URL of the image...'}),
         }
 
 def create_post_view(request):
@@ -25,6 +28,35 @@ def create_post_view(request):
     if request.method == 'POST':
         form = PostCreateForm(request.POST)
         if form.is_valid():
-            form.save()
+            post_form = form.save(commit=False)
+            
+            website = requests.get(form.data['url'])
+            html_code = BeautifulSoup(website.content, 'html.parser')
+            
+            # find_image = html_code.select('meta[content^="https://live.staticflickr.com/"]')
+            # image = find_image[0]['content']
+            # or
+            
+            try:
+                image = html_code.select('meta[content^="https://live.staticflickr.com/"]')[0]['content']
+            except:
+                messages.error(request, 'Invalid URL. Please try again.')
+                return redirect('create_post')
+            else:
+                post_form.image = image
+            
+            # find_title = html_code.select('h1.photo-title')
+            # title = find_title[0].text.strip()
+            # or
+            title = html_code.select('h1.photo-title')[0].text.strip()
+            post_form.title = title
+            
+            # find_artist = html_code.select('a.owner-name')
+            # artist = find_artist[0].text.strip()
+            # or
+            artist = html_code.select('a.owner-name')[0].text.strip()
+            post_form.artist = artist
+            
+            post_form.save()
             return redirect('home')
     return render(request, 'posts/create_post.html', {'form': form})
