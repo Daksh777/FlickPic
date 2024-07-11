9155d2a5f81fb5382c274d2cdf3db9fd86709e7f
Daksh P. Jain <jaindaksh42@gmail.com>
2024-07-11 16:17:56 +0530

    Add profile delete feature

--- users/views.py
+++ users/views.py
@@ -3,6 +3,8 @@ from .forms import *
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 from django.http import Http404
+from django.contrib.auth import logout
+from django.contrib import messages
 
 def profile_view(request, username=None):
     if username:
@@ -22,3 +24,13 @@ def profile_edit_view(request):
             form.save()
             return redirect('profile')
     return render(request, 'users/profile_edit.html', {'form': form})
+
+def profile_delete_view(request):
+    user = request.user
+    if request.method == 'POST':
+        logout(request)
+        user.delete()
+        messages.success(request, 'Your account has been deleted.')
+        return redirect('home')
+        
+    return render(request, 'users/profile_delete.html')
